605936860f5a91c4e63f374715d2c23272b38948
Marouane Fagri <mfagri@e2r8p13.1337.ma>
2022-05-23 13:44:13 +0100

    i am not a mybe i am the one

--- mandatory/builtins.c
+++ mandatory/builtins.c
@@ -6,7 +6,7 @@
 /*   By: mfagri <mfagri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 04:30:33 by mfagri            #+#    #+#             */
-/*   Updated: 2022/05/21 22:00:34 by mfagri           ###   ########.fr       */
+/*   Updated: 2022/05/23 13:42:50 by mfagri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,7 @@ int	ft_isalpha(int c)
 		return (0);
 	}
 }
-int ft_do_nothing(char **arg)
+int ft_do_nothing(char *arg)
 {
 	int j;
 	int a;
@@ -118,17 +118,17 @@ int ft_do_nothing(char **arg)
 	a = 0;
 	n = 0;
 	j = 0;
-	while (arg[1][j])
+	while (arg[j])
 	{
-		if(!(ft_isalpha(arg[1][j])) && (arg[1][j] != '_'))
+		if(!(ft_isalpha(arg[j])) && (arg[j] != '_'))
 		{
 			break;
 		}
-		if(ft_isalpha(arg[1][j]))
+		if(ft_isalpha(arg[j]))
 		{
 			a++;
 		}
-		if(arg[1][j] == '_')
+		if(arg[j] == '_')
 			n++;
 		j++;
 	}
@@ -138,98 +138,118 @@ int ft_do_nothing(char **arg)
 	}
 	return 0;
 }
-char **add_new(char **arg,char **env)
+void	add_env_str(char *arg,char **env)
+{
+	int i;
+	int n;
+//	char *f;
+//	int l;
+
+//	l = ft_strlen(arg);
+//	f = malloc(sizeof(char)*l+1);
+//	i = -1;
+////	while(arg[++i])
+//		f[i] = arg[i];
+////	f[i] = '\0';
+	i = 0;
+	while(env[i])
+	{
+		if(!ft_strncmp(env[i],arg,ft_strlen(arg)))
+			n = 1;
+		i++;
+	}
+	if (n == 1)
+		return;
+	else if (n != 1)
+	{
+		env[i] = cpy (env[i], arg);
+	}
+	env[i+1] = NULL;
+	return ;
+}
+void	add_new(char *arg,char **env)
 {
 	int i;
 	char *name;
 	char *value;
 	char *p;
-	char **ret;
 	char *f;
 	int j;
 	i = 0;
 
-	while(env[i])
-		i++;
-	ret = malloc(sizeof(char *) * (i + 2));
 	f = malloc(1);
 	p = malloc(1);
 	p[0] = '\0';
 	f[0] = '\0';
 	i = 0;
-	while (arg[1][i] != '=')
+	while (arg[i] != '=' && arg[i])
 		i++;
 	name = malloc(i + 1);
 	i =-1;
-	while (arg[1][++i] != '=')
-		name[i] = arg[1][i];
+	while (arg[++i] != '=' && arg[i])
+		name[i] = arg[i];
 	name[i] = '\0'; 
-		// printf("\n%s\n",name);
-	value = malloc(ft_strlen(arg[1]) - i + 1);
+	value = malloc(ft_strlen(arg) - i + 1);
 	j = 0;
 	i++;
-	if(arg[1][i] == '=')
+	if(arg[i] == '=')
 		i++;
-	while(arg[1][i])
-		value[j++] = arg[1][i++];
+	while(arg[i])
+		value[j++] = arg[i++];
 	value[i] = '\0';
-	// printf("\n%s\n",value);
 	f = ft_strjoin(f,name);
-	f = ft_strjoin(f,"=");
-	f = ft_strjoin(f,value);
+	if(strchr(arg,'='))
+		f = ft_strjoin(f,"=");
+	if(value[0])
+		f = ft_strjoin(f,value);
 	i = -1;
 	while(env[++i])
 	{
 		if(!ft_strncmp(env[i],name,ft_strlen(name)))
 		{
-			// j = 123456;
-			// ret[i] = cpy (ret[i], f);
-			++i;
+			j = 123456;
+			if(strchr(f,'='))
+				env[i] = cpy (env[i], f);
 		}
-		else
-			ret[i] = cpy (ret[i], env[i]);	
 	}
-	ret[i] = cpy (ret[i], f);
-	++i;
-	ret[i] = NULL;
-	return(ret);
+	if(j != 123456)
+	{
+		env[i] = cpy (env[i], f);
+		i++;
+	}
+	env[i] = NULL;
+	return ;
 }
-char **add_to_env(char **arg,char **env)
+void	add_to_env(char *arg,char **env)
 {
 	int i;
 	char *name;
 	char *value;
 	char *p;
-	char **ret;
 	char *f;
 	int j;
 	i = 0;
 
-	while(env[i])
-		i++;
-	ret = malloc(sizeof(char *) * (i + 2));
 	f = malloc(1);
 	p = malloc(1);
 	p[0] = '\0';
 	f[0] = '\0';
 	i = 0;
-	while (arg[1][i] != '+')
+	while (arg[i] != '+')
 		i++;
 	name = malloc(i + 1);
 	i =-1;
-	while (arg[1][++i] != '+')
-		name[i] = arg[1][i];
+	while (arg[++i] != '+')
+		name[i] = arg[i];
 	name[i] = '\0'; 
-		// printf("\n%s\n",name);
-	value = malloc(ft_strlen(arg[1]) - i + 1);
+	value = malloc(ft_strlen(arg) - i + 1);
 	j = 0;
 	i++;
-	if(arg[1][i] == '=')
+	if(arg[i] == '=')
 		i++;
-	while(arg[1][i])
-		value[j++] = arg[1][i++];
+	while(arg[i])
+		value[j++] = arg[i++];
 	value[i] = '\0';
-	// printf("\n%s\n",value);
 	f = ft_strjoin(f,name);
 	f = ft_strjoin(f,"=");
 	f = ft_strjoin(f,value);
@@ -239,37 +259,35 @@ char **add_to_env(char **arg,char **env)
 		if(!ft_strncmp(env[i],name,ft_strlen(name)))
 		{
 			j = 123456;
-			p = ft_strjoin(p,env[i]);
-			p = ft_strjoin(p,value);
+			env[i] = ft_strjoin(env[i],value);
 		}
-		else
-			ret[i] = cpy (ret[i], env[i]);	
 	}
 	if(j != 123456)
 	{
-		ret[i] = cpy (ret[i], f);
+		env[i] = cpy (env[i], f);
 		i++;
 	}
-	ret[i] = NULL;
-	return(ret);
+	env[i] = NULL;
+	return;
 }
-char **check_arg_export(char **arg,char **env)
+void check_arg_export(char *arg,char **env)
 {
 	int i;
 	int j;
 	int k;
 	j = 0;
 	i = 1;
-	if(!(ft_isalpha(arg[i][j])) && arg[1][j] != '_')
+	if(!(ft_isalpha(arg[0])) && arg[0] != '_')
 	{
 		ft_putstr_fd("export: `", 2);
-		ft_putstr_fd(arg[1], 2);
+		ft_putstr_fd(arg, 2);
 		ft_putstr_fd("': not a valid identifier\n", 2);
-		return (NULL);
+		return ;
 	}
-	while(arg[i][j])
+	i = 0;
+	while(arg[j])
 	{
-		if((ft_isalpha(arg[i][j])) || arg[i][j] == '_')
+		if((ft_isalpha(arg[j])) || arg[j] == '_')
 		{
 			j++;
 		}
@@ -277,62 +295,63 @@ char **check_arg_export(char **arg,char **env)
 			break;
 	}
 	if(ft_do_nothing(arg))
-		return(NULL);
+		return ;
 	else
-		if(arg[i][j] == '+')
+		if(arg[j] == '+')
 		{
-			if(arg[i][j+1] == '+')
+			if(arg[j+1] == '+')
 			{
 				ft_putstr_fd("minishell: ", 2);
 				ft_putstr_fd("export: `", 2);
-				ft_putstr_fd(arg[1], 2);
+				ft_putstr_fd(arg, 2);
 				ft_putstr_fd("': not a valid identifier\n", 2);
-				return(NULL);
+				return ;
 			}
-			env = add_to_env(arg,env);
+			add_to_env(arg,env);
 		}
-		else if (arg[i][j] == '=')
-			env = add_new(arg,env);
+		else if (arg[j] == '=')
+			add_new(arg,env);
+		else if (!(strchr(arg,'=')) && !(strchr(arg,'+')))
+		{
+			//add_env_str(arg,env);
+			add_new(arg,env);
+		}
 	i = 0;
-	while(env[i])
-		printf("%s\n",env[i++]);
-	return(env);
+	// while(env[i])
+	// 	printf("%s\n",env[i++]);
 }
-char **ft_export(char **env,char **arg)
+void	ft_export(char **env,char **arg)
 {
 	int i;
 	int j;
 	int k;
+	int p;
 	char **t;
-	char **ret;
 	i = 0;
 	i = 0;
 	while(env[i])
 		i++;
+	
 	t = malloc(sizeof(char *) * (i + 1));
-	ret = malloc(sizeof(char *) * (i + 1));
+	t[i] = NULL;
 	i = 0;
 	while(env[i])
 	{
-		j = 0 ;
+		j = -1;
 		k = 0;
-		while(env[j])
-		{
+		while(env[++j])
 			if(ft_strncmp(env[i],env[j],ft_strlen(env[i])) > 0)
 				k++;
-			j++; 
-		}
 		t[k] = cpy(t[k], env[i]);
-		ret[i] = cpy(ret[i], env[i]);
 		i++;
 	}
-	t[k + 1] = NULL;
-	ret[i + 1] = NULL;
-	i = 0;
-	if(!arg[1])
+	i = 1;
+	if(!arg[i])
 	{
+		i  = 0;
 		while(t[i])
 		{
+			p =0;
 			if (t[i][0])
 				write (1, "declare -x ", 12);
 			j = 0;
@@ -340,15 +359,21 @@ char **ft_export(char **env,char **arg)
 			{
 				write(1, &t[i][j], 1);
 				if(t[i][j] == '=')
+				{
+					p = 1;
 					write (1, "\"", 1);
+				}
+
 				j++;
 			}
-			write (1, "\"", 1);
+			if(p == 1)
+				write (1, "\"", 1);
 			write (1, "\n", 1);
 			i++;
 		}
 	}
 	else
-		ret = check_arg_export(arg,env);
-	return (ret);
+		while(arg[i])
+			check_arg_export(arg[i++],env);
+	return;
 }
